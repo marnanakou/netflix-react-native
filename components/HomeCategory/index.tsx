bead56a00c560b9ce7f2f4720f57fc049f0ba478
Maria Nanakou <marnanakou@gmail.com>
2021-11-05 23:38:19 +0100

    modify types Homescreen completed

--- components/HomeCategory/index.tsx
+++ components/HomeCategory/index.tsx
@@ -7,10 +7,10 @@ interface HomeCategoryProps {
     category: {
         id:string,
         title:string,
-        movies: [
+        movies: {
             id: string,
             poster:string,
-        ]
+        }[]
     }
 }
 
